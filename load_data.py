05f682347365bc7ad0ee777a9241252cb5f06192
saikatbsk <saikatbsk@gmail.com>
2017-08-08 08:36:51 +0530

    load data revised

--- load_data.py
+++ load_data.py
@@ -1,39 +1,55 @@
-import numpy as np
-
-#only for python 3.5 or above
+import os
 import glob
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Load_data:
-    def getNumberofClass():
-       n=0
-       for filename in glob.iglob('data/*.npy', recursive=True):
-        n += 1
-       
-       print(n)
-       return n
-    def load_data():
-        
-        n = 0
-        x_train = np.empty([1, 784])
-        y_train = np.empty([1, 1])
-        x_test = np.empty([1, 784])
-        y_test = np.empty([1, 1])
-        
-        for filename in glob.iglob('data/*.npy', recursive=True):
-            
-            data = np.load(filename)
-            data_x_train = data[ : (data.shape[0]*8//10) ]
-            data_x_test  = data[data.shape[0]*8//10 : ]
-            data_y_train = np.full((data_x_train.shape[0], 1), n)
-            data_y_test  = np.full((data_x_test.shape[0], 1), n)
-            print(filename,n)
-            n = n+1
-            print(x_train.shape)
-            print(data_x_train.shape)
-            x_train = np.concatenate((x_train, data_x_train), axis=0)
-            y_train = np.concatenate((y_train, data_y_train), axis=0)
-            x_test  = np.concatenate((x_test, data_x_test), axis=0)
-            y_test  = np.concatenate((y_test, data_y_test), axis=0)
-            
-        return x_train, y_train, x_test, y_test
-        
+def load_data(root, vfold_ratio=0.2, max_items_per_class=10000):
+    all_files = glob.glob(os.path.join(root, '*.npy'))
+
+    x = np.empty([0, 784])
+    y = np.empty([0])
+    class_names = []
+
+    for idx, file in enumerate(all_files):
+        data = np.load(file)
+        data = data[0:max_items_per_class, :]
+        labels = np.full(data.shape[0], idx)
+
+        x = np.concatenate((x, data), axis=0)
+        y = np.append(y, labels)
+
+        class_name, ext = os.path.splitext(os.path.basename(file))
+        class_names.append(class_name)
+
+    data = None
+    labels = None
+
+    permutation = np.random.permutation(y.shape[0])
+    x = x[permutation, :]
+    y = y[permutation]
+
+    vfold_size = int(x.shape[0]/100*(vfold_ratio*100))
+
+    x_test = x[0:vfold_size, :]
+    y_test = y[0:vfold_size]
+
+    x_train = x[vfold_size:x.shape[0], :]
+    y_train = y[vfold_size:y.shape[0]]
+
+    return x_train, y_train, x_test, y_test, class_names
+
+def visualize(X, Y, classes, samples_per_class=10):
+    nb_classes = len(classes)
+
+    for y, cls in enumerate(classes):
+        idxs = np.flatnonzero(Y == y)
+        idxs = np.random.choice(idxs, samples_per_class, replace=False)
+
+        for i, idx in enumerate(idxs):
+            plt_idx = i * nb_classes + y + 1
+            plt.subplot(samples_per_class, nb_classes, plt_idx)
+            plt.imshow(X[idx], cmap='gray')
+            plt.axis('off')
+            if i == 0:
+                plt.title(cls)
+    plt.show()
--- main.py
+++ main.py
@@ -1,118 +1,106 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-from keras.callbacks import ModelCheckpoint
+# import keras
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Flatten
+# from keras.layers import Conv2D, MaxPooling2D
+# from keras import backend as K
+# from keras.callbacks import ModelCheckpoint
+from load_data import *
 
-import load_data as data
+# epochs = 20
+# input_shape = (28, 28, 1)
+# batch_size = 128
 
-epochs = 20
-num_classes= data.Load_data.getNumberofClass()
-input_shape = ( 28, 28, 1)
-batch_size = 128
+x_train, y_train, x_test, y_test, class_names = load_data('data')
 
-x_train, y_train, x_test, y_test = data.Load_data.load_data()
+# Reshape and normalize
+x_train = x_train.reshape(x_train.shape[0], 28, 28).astype('float32')
+x_test = x_test.reshape(x_test.shape[0], 28, 28).astype('float32')
 
-x_train = x_train.astype('float32')
-y_train = y_train.astype('float32')
-x_test  = x_test.astype('float32')
-y_test  = y_test.astype('float32')
-'''
-image = x_train[130]
-print(y_train[130])
-image = np.reshape(image, ( 28, 28))
-imgplot = plt.imshow(image, cmap='gray')
-plt.show()
+x_train /= 255.0
+x_test /= 255.0
 
-'''
-x_train = np.reshape(x_train, (x_train.shape[0], 28, 28, 1))
-x_test = np.reshape(x_test, (x_test.shape[0], 28, 28, 1))
+print('Training data: ', x_train.shape)
+print('Training labels: ', y_train.shape)
+print('Test data: ', x_test.shape)
+print('Test labels: ', y_test.shape)
+print('Class names:', class_names)
 
-x_train /= 255
-x_test  /= 255
+plt.figure(figsize=(10, 10))
+visualize(x_train, y_train, class_names)
 
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-
-
-
-#Model definition
-model = Sequential()
-
-model.add(Conv2D(6, kernel_size=(3, 3), activation='relu', input_shape=input_shape, padding="same"))
-model.add(MaxPooling2D(pool_size=(2, 2)))                 
-model.add(Conv2D(16, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(120, (3, 3), activation='relu'))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(84, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-
-print(model.summary())
-
-#For saving weight
-save_filename = 'weights.h5'
-callback_period = 5
-verbose = 1
-if not os.path.exists(save_filename):
-    # Model checkpoint callback
-    checkpoint = ModelCheckpoint(
-        save_filename,
-        monitor='val_acc',
-        verbose=verbose,
-        save_best_only=True,
-        save_weights_only=False,
-        mode='auto',
-        period=callback_period)
-
-    # Fit the model
-    history = model.fit(
-        x_train, y_train,
-        epochs=epochs, batch_size=batch_size, verbose=verbose,
-        validation_data=(x_test, y_test),
-        callbacks=[checkpoint])
-
-    # List all data in history
-    print(history.history.keys())
-
-    # Plot history for accuracy
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('img/acc.png')
-    plt.clf()
-
-    # Plot history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('img/loss.png')
-else:
-    # Load previously saved weights and evaluate the model
-    model.load_weights(save_filename)
-    score, acc = model.evaluate(x_test, y_test, verbose=0)
-    print('Test score:', score)
-    print('Test accuracy:', acc)
-
+# # convert class vectors to binary class matrices
+# num_classes = len(class_names)
+# y_train = keras.utils.to_categorical(y_train, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
+#
+# #Model definition
+# model = Sequential()
+#
+# model.add(Conv2D(6, kernel_size=(3, 3), activation='relu', input_shape=input_shape, padding="same"))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(16, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(120, (3, 3), activation='relu'))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(84, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(num_classes, activation='softmax'))
+#
+# model.compile(loss=keras.losses.categorical_crossentropy,
+#               optimizer=keras.optimizers.Adadelta(),
+#               metrics=['accuracy'])
+#
+# print(model.summary())
+#
+# #For saving weight
+# save_filename = 'weights.h5'
+# callback_period = 5
+# verbose = 1
+# if not os.path.exists(save_filename):
+#     # Model checkpoint callback
+#     checkpoint = ModelCheckpoint(
+#         save_filename,
+#         monitor='val_acc',
+#         verbose=verbose,
+#         save_best_only=True,
+#         save_weights_only=False,
+#         mode='auto',
+#         period=callback_period)
+#
+#     # Fit the model
+#     history = model.fit(
+#         x_train, y_train,
+#         epochs=epochs, batch_size=batch_size, verbose=verbose,
+#         validation_data=(x_test, y_test),
+#         callbacks=[checkpoint])
+#
+#     # List all data in history
+#     print(history.history.keys())
+#
+#     # Plot history for accuracy
+#     plt.plot(history.history['acc'])
+#     plt.plot(history.history['val_acc'])
+#     plt.title('model accuracy')
+#     plt.ylabel('accuracy')
+#     plt.xlabel('epoch')
+#     plt.legend(['train', 'test'], loc='upper left')
+#     plt.savefig('img/acc.png')
+#     plt.clf()
+#
+#     # Plot history for loss
+#     plt.plot(history.history['loss'])
+#     plt.plot(history.history['val_loss'])
+#     plt.title('model loss')
+#     plt.ylabel('loss')
+#     plt.xlabel('epoch')
+#     plt.legend(['train', 'test'], loc='upper left')
+#     plt.savefig('img/loss.png')
+# else:
+#     # Load previously saved weights and evaluate the model
+#     model.load_weights(save_filename)
+#     score, acc = model.evaluate(x_test, y_test, verbose=0)
+#     print('Test score:', score)
+#     print('Test accuracy:', acc)
